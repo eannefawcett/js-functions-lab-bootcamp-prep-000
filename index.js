0368797f0dcb125ab983c6a7da5f33126ea74166
Elizabeth Fawcett <elizabethfawcett47@gmail.com>
2019-06-25 17:28:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown (holiday, days) {
-  return `It's ${days} days until ${holiday}`
+  return `It's ${days} days until ${holiday}!`
 }
