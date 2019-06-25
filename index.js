ed1864e0818aee2d3bac1cd81f02dd9ec7ac5d6f
Elizabeth Fawcett <elizabethfawcett47@gmail.com>
2019-06-25 17:22:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // write your code below!
+var name = "Beth"
+var holiday = "Halloween"
+
 function happyHolidays () {
 return "Happy holidays!"
+}
+function happyHolidaysTo (name, holdiay) {
+  return `Happy ${holiday}, ${name}!`
 }
